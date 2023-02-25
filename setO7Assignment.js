3beb1b1884df3d37c973d03ad25bcc65a46fab49
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-25 11:39:26 +0100

    update

--- setO7Assignment.js
+++ setO7Assignment.js
@@ -3,8 +3,8 @@
 // afternoon = 12pm - {15pm or 3pm}
 // evening = {16pm or 6} - {23pm or 11pm}
 
-let check = 24;
-let check_Morning__Afternoon_And_Evening = "pm";
+let check = 24; // check time
+let check_Morning__Afternoon_And_Evening = "pm"; // compare when it morning, afternon and evening
 
 if (
   check <= 12 &&
